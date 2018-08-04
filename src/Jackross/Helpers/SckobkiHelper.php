2d341b14aaa72b5f8a35dd411bd619c71ca8d87c
JacRoss <tori.belechy@gmail.com>
2018-08-04 23:50:49 +0300

    first commit

---
+++ src/Jackross/Helpers/SckobkiHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Jackross\Helpers;
+
+/**
+ * Class SkobkiHelper
+ * @package Jackross\Helpers
+ */
+class SckobkiHelper
+{
+    /**
+     * @param string $text
+     * @return bool
+     */
+    public static function validate(string $text): bool
+    {
+        if (mb_stripos('(', $text) && mb_stripos(')', $text)) {
+            throw new \InvalidArgumentException();
+        }
+
+        $count = 0;
+
+        for ($i = 0; $i < mb_strlen($text); $i++) {
+            $symbol = mb_substr($text, $i, 1);
+
+            if ($symbol === '(') {
+                $count++;
+            }
+
+            if ($symbol === ')') {
+                $count--;
+            }
+
+            if ($count < 0) {
+                break;
+            }
+        }
+
+        return $count === 0;
+    }
+}
+
+
+
---
+++ tests/SckobkiHelperTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Jackross\Tests;
+
+use Jackross\Helpers\SckobkiHelper;
+use PHPUnit\Framework\TestCase;
+
+class SckobkiHelperTest extends TestCase
+{
+
+    public function testValidBrackets()
+    {
+      $this->assertTrue(SckobkiHelper::validate('((pam)())((({PIP})))(()(param))'));
+    }
+
+    public function testInvalidBrackets()
+    {
+        $this->assertTrue(SckobkiHelper::validate('(()()()()))((((()()()))(()()()(((()))))))'));
+    }
+
+    public function testInvalidBracketsArgument()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        SckobkiHelper::validate('pam param pam');
+    }
+
+}
