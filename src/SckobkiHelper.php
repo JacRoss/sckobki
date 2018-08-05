df3a8b2f88cfb0b60c7126c68a8ebe5411ec9dfe
JacRoss <tori.belechy@gmail.com>
2018-08-05 15:19:05 +0300

    -

--- src/SckobkiHelper.php
+++ src/SckobkiHelper.php
@@ -11,16 +11,19 @@ class SckobkiHelper
     /**
      * @param string $text
      * @return bool
+     *
+     * @throws \InvalidArgumentException
      */
     public static function validate(string $text): bool
     {
-        if (mb_stripos('(', $text) && mb_stripos(')', $text)) {
+        if (mb_stripos('(', $text) === false || mb_stripos(')', $text) === false) {
             throw new \InvalidArgumentException();
         }
 
         $count = 0;
+        $len  = mb_strlen($text);
 
-        for ($i = 0; $i < mb_strlen($text); $i++) {
+        for ($i = 0; $i < $len; $i++) {
             $symbol = mb_substr($text, $i, 1);
 
             if ($symbol === '(') {
